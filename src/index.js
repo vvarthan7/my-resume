e00f2abb1a68f9870915183754c6fb3276fbbf55
Vishnu Varthan <vvarthan7@gmail.com>
2018-05-01 13:30:15 +0530

    renamed files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Resume from './Resume';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Resume />, document.getElementById('root'));
 registerServiceWorker();
